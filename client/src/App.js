93f72ead46f6db4adbf127e226aeeb7c40e8adf8
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-03-31 08:45:50 -0300

    Corrección

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link } from 'react-router-dom';
 
+
 function App() {
   return (
     <BrowserRouter>
       <h1>Hola</h1>
       <p>
-        <link to= "/">Home</link>
-        <link to= "/about">About</link>
+        <Link to= "/">Home </Link>
+        <Link to= "/about">About</Link>
       </p>
     </BrowserRouter>  
   );
